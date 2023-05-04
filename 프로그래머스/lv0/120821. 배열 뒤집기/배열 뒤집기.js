e149d45630641ffb6ed8bae00411b0532c6c32ec
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-04 14:08:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    
+    return num_list.reverse();
+    
+    // 반복문
+    // let result = [];
+    // for (let num of num_list) {
+    //     result.unshift(num);
+    // }
+    // return result;
+}
