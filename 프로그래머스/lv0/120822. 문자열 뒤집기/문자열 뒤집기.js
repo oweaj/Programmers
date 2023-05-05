026d9f97f3159b43fc1ca84a8b87d89169570267
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-05 12:37:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+        
+    return my_string.split("").reverse().join("");
+    
+    // let result = [];
+    // for(str of my_string) {
+    //     result.unshift(str);
+    // }
+    // return result.join("");
+}
