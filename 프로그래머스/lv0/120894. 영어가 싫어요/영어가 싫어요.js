7a508933d56ef56f6adc8dec2bb49027f5fda34e
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-16 17:10:18 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for (let i = 0; i < arr.length; i++) {
+        numbers = numbers.split(arr[i]).join(i)
+    }
+    
+    return Number(numbers);
+}
