cdcba91474423d8a0f166e3c3b549318fdad6d9c
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-18 13:40:19 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,5 @@
 function solution(num, k) {
     let str = String(num)
     
-    return str.includes(k) ? str.indexOf(String(k)) + 1 : -1
+    return str.includes(String(k)) ? str.indexOf(String(k)) + 1 : -1
 }
