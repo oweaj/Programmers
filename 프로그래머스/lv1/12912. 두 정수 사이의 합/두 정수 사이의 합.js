2025a2e238c1d5db97f7971de8abeef0d5736bce
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-22 13:42:40 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    let result = 0;
+    
+    if (a > b) [a, b] = [b, a];
+    
+    for (let i = a; i <= b; i++) {
+       result += i;
+    }
+    
+    return result;
+}
