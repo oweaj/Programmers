dfdb8f3ce09c08342cb7140d05a2cc2563caee30
oweaj <101049520+oweaj@users.noreply.github.com>
2023-05-31 23:46:23 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    let min = a > b ? b : a
+    let checkNum;
+    
+    for (let i = 1; i <= min; i++) {
+        if(a % i === 0 && b % i === 0) checkNum = i
+    }
+
+    let result = b/checkNum
+    
+    while(result % 2 === 0) result /= 2
+    
+    while(result % 5 === 0) result /= 5
+    
+    return result === 1 ? 1 : 2; 
+}
