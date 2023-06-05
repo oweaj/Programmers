e336164082938ca1ce1f7f01f07fbef446192f01
oweaj <101049520+oweaj@users.noreply.github.com>
2023-06-06 00:04:48 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let num = 0
+    
+    for (let i = 1; i <= n; i++) {
+        num++;
+        while (num % 3 === 0 || num.toString().includes("3")) {
+            num++
+        }
+    }
+    
+    return num;
+}
