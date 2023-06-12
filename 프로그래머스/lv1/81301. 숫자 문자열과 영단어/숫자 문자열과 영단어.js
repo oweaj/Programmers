f5d6a12623719651011bcd50702e74dc1db31d38
oweaj <101049520+oweaj@users.noreply.github.com>
2023-06-12 23:35:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    let word = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    for (let i = 0; i < word.length; i++) {
+        let temp = s.split(word[i])
+        s = temp.join(i)     
+    }
+    
+    return Number(s);
+}
