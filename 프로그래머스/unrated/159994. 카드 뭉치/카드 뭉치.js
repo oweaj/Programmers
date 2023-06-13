a50554f249abfb28d92dcd9626e797b3ddaa9a35
oweaj <101049520+oweaj@users.noreply.github.com>
2023-06-13 13:11:44 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,11 @@
+function solution(cards1, cards2, goal) {
+    let temp = []
+    
+    for (let item of goal) {
+        if (item === cards1[0]) cards1.shift()
+        else if (item === cards2[0]) cards2.shift()
+        else return "No"
+    }
+    
+    return "Yes";
+}
