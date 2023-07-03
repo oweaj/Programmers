b5b0b3e7e69b8530681d109f51f200b939caa897
oweaj <101049520+oweaj@users.noreply.github.com>
2023-07-03 21:16:49 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,10 +1,3 @@
 function solution(num_list) {
-    let sort = num_list.sort((a, b) => a - b)
-    let result = []
-    
-    for (let i = 5; i < sort.length; i++) {
-        result.push(sort[i])
-    }
-    
-    return result;
+    return num_list.sort((a, b) => a - b).slice(5)
 }
