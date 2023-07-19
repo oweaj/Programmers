22677ca1288f3033da8873876b7da942aefeaf22
oweaj <101049520+oweaj@users.noreply.github.com>
2023-07-19 23:52:52 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -1,9 +1,13 @@
 function solution(names) {
-    let result = []
+//     let result = []
     
-    for (let i = 0; i < names.length; i += 5) {
-        result.push(names[i])
-    }
+//     for (let i = 0; i < names.length; i += 5) {
+//         result.push(names[i])
+//     }
     
-    return result;
+//     return result
+    
+    
+    // 다른방법 filter로 풀기
+    return names.filter((el, i) => i % 5 === 0)
 }
