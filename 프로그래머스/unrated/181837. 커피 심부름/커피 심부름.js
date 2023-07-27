e775e5d92944533f35c41083b776b3e0ce4dee6e
oweaj <101049520+oweaj@users.noreply.github.com>
2023-07-27 21:00:47 +0900

    [unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -2,12 +2,9 @@ function solution(order) {
     let result = 0
     
     for (let x of order) {
-        if ( x === "icecafelatte" || x === "cafelatteice" || x === "hotcafelatte" || x === "cafelattehot" || x === "cafelatte") {
-            result += 5000
-        } else {
-            result += 4500
-        }
+        if (x.includes("cafelatte")) result += 5000
+        else result += 4500
     }
-
+    
     return result
 }
