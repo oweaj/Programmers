df68dceeb5c486ecaf91c4d0d6a913a203629ea5
oweaj <101049520+oweaj@users.noreply.github.com>
2023-08-29 20:57:45 +0900

    [unrated] Title: 배열 조각하기, Time: 0.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,8 +1,8 @@
 function solution(arr, query) {
     
     query.forEach((el, i) => {
-        if (i % 2 === 0) arr.splice(el + 1, arr.length - (el + 1))
-        else arr.splice(0, el)        
+        if (i % 2) arr.splice(0, el)
+        else arr.splice(el + 1) 
     })
 
     return arr;
