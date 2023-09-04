bba0215993a6283e2f7dbb8d252f5bdfb76cf73b
oweaj <101049520+oweaj@users.noreply.github.com>
2023-09-05 00:12:16 +0900

    [unrated] Title: 1로 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let count = 0
+    
+    for (let num of num_list) {
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num = num / 2
+                count++
+            } else {
+                num = (num - 1) / 2
+                count++
+            }
+        }
+    }
+
+    return count;
+}
