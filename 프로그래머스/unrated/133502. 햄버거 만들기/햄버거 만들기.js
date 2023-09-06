b21792dac222f4232674c7935c558fe81bbc58d8
oweaj <101049520+oweaj@users.noreply.github.com>
2023-09-06 22:42:48 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -5,11 +5,14 @@ function solution(ingredient) {
     for (let x of ingredient) {
         temp.push(x)
         
-        let check = temp.slice(-4).join("")
-        if (check === "1231") {
-            count++
-            temp.splice(-4)
+        if (temp.length >= 4) {
+            let check = temp.slice(-4).join("")
+            if (check === "1231") {
+                count++
+                temp.splice(-4)
+            }
         }
+        
     }
         
     return count;
