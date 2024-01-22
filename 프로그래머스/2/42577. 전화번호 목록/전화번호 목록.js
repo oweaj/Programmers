3ac772f489d7ed10184df97958226fe739413fcc
oweaj <101049520+oweaj@users.noreply.github.com>
2024-01-22 16:13:17 +0900

    [level 2] Title: 전화번호 목록, Time: 140.93 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,29 @@
 function solution(phone_book) {
-    const hash = {}
+    const sort = phone_book.sort()
     
-    for (let num of phone_book) {
-        hash[num] = true
+    for (let i = 0; i < sort.length - 1; i++) {
+        if (sort[i] === sort[i + 1].slice(0, sort[i].length)) {
+            return false
+        }
     }
     
-    for (let item of phone_book) {
-        for (let i = 1; i < item.length; i++) {
-            const check = item.slice(0, i)
-            if (hash[check]) return false
-        }    
-    }
-
-    return true;
+    return true
 }
+
+// hash 풀이
+// function solution(phone_book) {
+//     const hash = {}
+    
+//     for (let num of phone_book) {
+//         hash[num] = true
+//     }
+    
+//     for (let item of phone_book) {
+//         for (let i = 1; i < item.length; i++) {
+//             const check = item.slice(0, i)
+//             if (hash[check]) return false
+//         }    
+//     }
+
+//     return true;
+// }
