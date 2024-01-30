86c7194782d37b7b420187dda8c01a934df5f22a
oweaj <101049520+oweaj@users.noreply.github.com>
2024-01-30 14:07:17 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,18 +1,14 @@
 function solution(clothes) {
-    let map = new Map()
     let result = 1
+    let temp = {}
     
     for (let item of clothes) {
-        if (map.has(item[1])) {
-            map.set(item[1], map.get(item[1]) + 1)    
-        } else {
-            map.set(item[1], 1)
-        }
+        temp[item[1]] = (temp[item[1]] || 0) + 1
     }
     
-    for (let [key, value] of map) {
-        result *= value + 1
+    for (let key in temp) {
+        result *= temp[key] + 1
     }
     
-    return result - 1   
+    return result - 1
 }
