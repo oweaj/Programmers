3f9255763df6197b86b1cc7e9de719c113c1fc37
oweaj <101049520+oweaj@users.noreply.github.com>
2024-02-17 00:06:31 +0900

    [level 2] Title: 숫자의 표현, Time: 1.64 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,20 @@
 function solution(n) {
-    let count = 0
+    let count = 1
     
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0 && i % 2 === 1) count++
+    for (let i = 1; i < n; i++) {  
+        let sum = 0
+        for (let j = i; j < n; j++) {
+            sum += j;
+            if (sum === n) {
+                count++;
+                sum = 0;
+                break
+            } else if (sum > n) {
+                sum = 0;
+                break
+            }
+        } 
     }
-   
+
     return count;
 }
